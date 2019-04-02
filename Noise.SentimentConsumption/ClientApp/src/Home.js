a97448a5a767c42c61dc3fea9d38f1bffbc512fe
Brian <beerye432@gmail.com>
2019-04-01 22:30:24 -0700

    Playing around with grid and CSS

--- Noise.SentimentConsumption/ClientApp/src/Home.js
+++ Noise.SentimentConsumption/ClientApp/src/Home.js
@@ -6,8 +6,17 @@ export default class Home extends Component {
     static displayName = Home.name;
 
     render() {
+        let grid = Array.from(Array(512).keys()).map(function (item, index) {
+            let color = "rgb(" + index + "," + index / 3 + "," + index / 2 + ")";
+            let s = {
+                color: color,
+                backgroundColor: color
+            }
+            return <div key={index} style={s} className={"grid-cell"}></div>
+        });
+
         return (
-            <div className="noise-grid"></div>
+            <div className="noise-grid">{grid}</div>
         );
     }
 }
